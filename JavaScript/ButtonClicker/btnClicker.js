37bbae0e0795ba956f734b4dc7f2dd7396f2a58b
GhostExabyte <haglenn78@gmail.com>
2022-06-27 22:55:27 -0400

    all

---
+++ JavaScript/ButtonClicker/btnClicker.js
@@ -0,0 +1,8 @@
+function removeElement (element){
+    console. log ("remove");
+    element. remove ();
+}
+function Login(element){
+    console.log("logged in",  element);
+    element.innerText = "Logout"
+}
---
+++ JavaScript/PUP/puppyGram/pup.js
@@ -0,0 +1,5 @@
+
+function playVideo(element){
+    console.log("playvideo");
+    element.play();
+}
